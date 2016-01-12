31bd22f64761fc77eaa45f5b44d13593f6540b39
Taylor Young <taylor@hyprtxt.com>
2016-01-12 13:40:05 -0700

    Closes #2

--- comments.php
+++ comments.php
@@ -1,19 +1,20 @@
 <?php
 if ( is_single() || is_page() ) :
   echo '<div class="clearfix"></div>';
-  if ( have_comments() && comments_open() ) : ?>
+  if ( comments_open() ) : ?>
     <h4 id="comments"><?php comments_number( __( 'Leave a Comment', 'bootstrap-four' ), __( 'One Comment', 'bootstrap-four' ), '%' . __( ' Comments', 'bootstrap-four' ) );?></h4>
+  <?php
+  endif;
+  if ( have_comments() ) : ?>
     <ul class="commentlist">
       <?php wp_list_comments(); ?>
       <?php paginate_comments_links(); ?>
       <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
     </ul>
-<?php
+  <?php
+  endif;
+  if ( comments_open() ) :
     comment_form();
-  else :
-    if ( comments_open() ) :
-      comment_form();
-    endif;
   endif;
 endif;
 ?>
